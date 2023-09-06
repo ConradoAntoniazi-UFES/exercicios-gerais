b4c16fe7cc2e90e08bd707784d15a7379be3c4c6
casantos <conrado.santos@edu.ufes.br>
2023-09-05 23:12:44 -0300

    att biblio_03

---
+++ 03_bibliotecas/bibli_03/Respostas/Conrado/data.c
@@ -0,0 +1,43 @@
+#include "data.h"
+#include <stdio.h>
+
+int verificaBissexto(int ano) {
+    if(ano % 4 == 0 && ano % 100 != 0) return 1;
+    else if(ano % 100 == 0 && ano % 400 == 0) return 1;
+
+    return 0;
+}
+
+int verificaDataValida(int dia, int mes, int ano) {
+    if(verificaBissexto(ano) && mes == 2 && (dia > 0 && dia <= 29)) return 1;
+    else if(!verificaBissexto(ano) && mes == 2 && (dia > 0 && dia <= 28)) return 1;
+    else if((mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes ==12) && (dia > 0 && dia <= 31) && ano >= 0) return 1;
+    else if((mes == 4 || mes == 6 || mes == 9 || mes == 11) && (dia > 0 && dia <= 30) && ano >= 0) return 1;
+
+    return 0;
+}
+
+void imprimeMesExtenso(int mes) {
+    if(mes == 1) printf("janeiro");
+    else if(mes == 2) printf("fevereiro");
+    else if(mes == 3) printf("marco");
+    else if(mes == 4) printf("abril");
+    else if(mes == 5) printf("maio");
+    else if(mes == 6) printf("junho");
+    else if(mes == 7) printf("julho");
+    else if(mes == 8) printf("agosto");
+    else if(mes == 9) printf("setembro");
+    else if(mes == 10) printf("outubro");
+    else if(mes == 11) printf("novembro");
+    else if(mes == 12) printf("dezembro");
+}
+
+void imprimeDataExtenso(int dia, int mes, int ano) {
+    printf("%d de ", dia);
+    imprimeMesExtenso(mes);
+    printf(" de %d\n", ano);
+}
+
+int numeroDiasMes(int mes, int ano) {
+    
+}
