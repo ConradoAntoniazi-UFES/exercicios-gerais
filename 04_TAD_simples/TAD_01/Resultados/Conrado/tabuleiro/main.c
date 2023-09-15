145c928e5d04407f2886f00c024e4d2609d9c94e
casantos <conrado.santos@edu.ufes.br>
2023-09-15 17:21:19 -0300

    att tad 1

---
+++ 04_TAD_simples/TAD_01/Resultados/Conrado/tabuleiro/main.c
@@ -0,0 +1,14 @@
+#include "jogo.h"
+#include <stdio.h>
+
+int main() {
+    tJogo jogo;
+
+    jogo = CriaJogo();
+
+    do {
+        ComecaJogo(jogo);
+    } while (ContinuaJogo());
+
+    return 0;
+}
