a473e52624d1d39855f7962f55e7137ec3fb0bfa
casantos <conrado.santos@edu.ufes.br>
2023-09-28 15:00:38 -0300

    pont 04 intermediario

---
+++ 05_ponteiros/pont_04/Respostas/Conrado/utils.c
@@ -0,0 +1,15 @@
+#include "utils.h"
+#include <stdio.h>
+
+void LeNumeros(int *array, int tamanho) {
+    
+    for(int i = 0; i < tamanho; i++) {
+        scanf("%d", &array[i]);
+    }
+}
+
+void EncontraMaiorMenorMedia(int *array, int tamanho, int *maior, int *menor, float *media) {
+    for(int i = 0; i < tamanho; i++) {
+        
+    }
+}
