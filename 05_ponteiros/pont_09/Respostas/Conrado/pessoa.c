9183a8cdd453c8b2ddc8fe9ea941df91f674bd8c
casantos <conrado.santos@edu.ufes.br>
2023-10-03 11:54:03 -0300

    pont 09 - não completo

---
+++ 05_ponteiros/pont_09/Respostas/Conrado/pessoa.c
@@ -0,0 +1,58 @@
+#include "pessoa.h"
+#include <stdio.h>
+
+tPessoa CriaPessoa() {
+    tPessoa pesssoa;
+
+    pesssoa.nome[0] = '\0';
+    pesssoa.pai = NULL;
+    pesssoa.mae = NULL;
+
+    return pesssoa;
+}
+
+void LePessoa(tPessoa *pessoa) {
+    *pessoa = CriaPessoa();
+
+    scanf("%*[^A-Za-z]");
+    scanf("%[^\n]", pessoa->nome);
+}
+
+int VerificaSeTemPaisPessoa(tPessoa *pessoa) {
+    if(pessoa->pai != NULL || pessoa->mae != NULL) return 1;
+
+    return 0;
+}
+
+void ImprimeNome(tPessoa *pessoa) {
+    int i = 0;
+
+    while (pessoa->nome[i] != '\0') {
+        printf("%c", pessoa->nome[i]);
+    }
+}
+
+void ImprimePessoa(tPessoa *pessoa) {
+    if(VerificaSeTemPaisPessoa(pessoa)) {
+        printf("NOME COMPLETO: ");
+        ImprimeNome(pessoa);
+        
+        printf("PAI: ");
+        if(pessoa->pai != NULL) {
+            ImprimeNome(pessoa->pai);
+        } else {
+            printf("NAO INFORMADO\n");
+        }
+
+        printf("MAE: ");
+        if(pessoa->mae != NULL) {
+            ImprimeNome(pessoa->mae);
+        } else {
+            printf("NAO INFORMADO\n");
+        }
+    }
+}
+
+void AssociaFamiliasGruposPessoas(tPessoa *pessoas) {
+
+}
